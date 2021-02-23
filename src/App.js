e0ae39cd938ac18c07e69f6d297436d8ee99b0ac
konan2610 <konan.maruyama@archibase.co.jp>
2021-02-24 01:01:41 +0900

    Fix error on Github pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Jobs } from './containers/Jobs'
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/">
           <Top />
